4f195ed81d15ab4cde67ee24f46945ad12eab4d2
wxd <413586280@qq.com>
2022-07-17 23:38:12 +0800

    Update main.go

--- etcd/main.go
+++ etcd/main.go
@@ -1,16 +1,5 @@
 package main
 
-/*
- * @Descripttion: 828 project
- * @Version: 1.0.0
- * @Author: wangxiaodiao
- * @Email: 413586280@qq.com
- * @Date: 2021-11-01 18:52:17
- * @LastEditors: wangxiaodiao
- * @LastEditTime: 2022-07-17 22:18:23
- * @FilePath: /vcg-global/app/auctionmgr/main.go
- */
-
 import (
 	"log"
 
